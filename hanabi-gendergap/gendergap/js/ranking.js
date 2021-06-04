f6efdeedd33963c30d5390337ae46ebac1760e8a
Jenscode-trash <71389754+jjenscodee@users.noreply.github.com>
2021-06-04 10:54:14 +0200

    fix select problem & color

--- hanabi-gendergap/gendergap/js/ranking.js
+++ hanabi-gendergap/gendergap/js/ranking.js
@@ -115,9 +115,9 @@ function plotChart(data, continent) {
 
     let sliderValue = document.getElementById("slVal1");
     let inputSlider = document.getElementById("slIn1");
-    d3.selectAll("rect").remove();
+    d3.selectAll(".rankrect").remove();
     //d3.selectAll("image").remove();
-    d3.selectAll("text").remove();
+    d3.selectAll(".ranktext").remove();
 
     const svg = d3.select("#chart")
     const width = 500;
@@ -154,6 +154,7 @@ function plotChart(data, continent) {
             .attr("class", (d, i) => d.key)
             .attr("x", 0)
             .attr("y", 0)
+            .attr("class", "ranktext")
             .style("opacity", 0)
             .style("fill", "white")
             
@@ -162,6 +163,7 @@ function plotChart(data, continent) {
             .data(presentData)
             .enter()
             .append("rect")
+            .attr("class", "rankrect")
             .attr("id", (d, i) => "rect" + sortedRange.findIndex(e => e.key === d.key))
             .style("fill", d => findColor(d.value2))
             .style("opacity", 0.95)
@@ -218,16 +220,16 @@ function plotChart(data, continent) {
         container
             .selectAll("rect")
             .on("mouseover", function (d, i) {
-                d3.select(this).style("opacity", 1)
+                d3.select(this).style("fill", d => findColor(d.value2)).style("opacity", 0.4)
             })
             .on("mouseout", function (d, i) {
-                d3.select(this).style("opacity", 0.7)
+                d3.select(this).style("fill", d => findColor(d.value2)).style("opacity", 0.95)
             })
             .on("click", function (d, i) {
-                d3.selectAll("text").style("opacity", 0);
+                d3.selectAll(".ranktext").style("opacity", 0);
 
 
-                d3.selectAll("rect")
+                d3.selectAll(".rankrect")
                     .attr("x", d => d.value1 * 10 <= 0 ? 500 - 0 / 2 : 500 - (widthScale(d.value1) / 2) / 2 * 1.6)
                     .attr("y", (d, i) => sortedRange.findIndex(e => e.key === d.key) * (rectProperties.height + rectProperties.padding))
                     .attr("width", d => d.value1 * 10 <= 0 ? 0 : widthScale(d.value1) / 2 * 1.6)
